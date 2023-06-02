ea4d69dc3820b646991a50f221053c601703c955
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-02 18:22:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
-// Your code here.
+document.addEventListener('DOMContentLoaded', () => {
+  const itemsContainer = document.querySelector('.items');
+  let isDragging = false;
+  let startPosition = 0;
+  let startScrollLeft = 0;
 
-    document.addEventListener('DOMContentLoaded', () => {
-      const itemsContainer = document.querySelector('.items');
-      let isDragging = false;
-      let startPosition = 0;
-      let startScrollLeft = 0;
+  itemsContainer.addEventListener('mousedown', (event) => {
+    isDragging = true;
+    startPosition = event.pageX;
+    startScrollLeft = itemsContainer.scrollLeft;
+  });
 
-      itemsContainer.addEventListener('mousedown', (event) => {
-        isDragging = true;
-        startPosition = event.pageX;
-        startScrollLeft = itemsContainer.scrollLeft;
-      });
+  itemsContainer.addEventListener('mousemove', (event) => {
+    if (!isDragging) return; // If not dragging, ignore the event
+    event.preventDefault();
+    const distance = event.pageX - startPosition;
+    itemsContainer.scrollLeft = startScrollLeft - distance;
+  });
 
-      itemsContainer.addEventListener('mousemove', (event) => {
-        if (!isDragging) return;
-        event.preventDefault();
-        const distance = event.pageX - startPosition;
-        itemsContainer.scrollLeft = startScrollLeft - distance;
-      });
+  const endDragging = () => {
+    isDragging = false;
+  };
 
-      itemsContainer.addEventListener('mouseup', () => {
-        isDragging = false;
-      });
-
-      itemsContainer.addEventListener('mouseleave', () => {
-        isDragging = false;
-      });
-    });
-  
+  itemsContainer.addEventListener('mouseup', endDragging);
+  itemsContainer.addEventListener('mouseleave', endDragging);
+});
