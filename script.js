411f9e0de8f278823897b3edc86944d7d87722ed
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-02 16:01:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // Your code here.
+
+    document.addEventListener('DOMContentLoaded', () => {
+      const itemsContainer = document.querySelector('.items');
+      let isDragging = false;
+      let startPosition = 0;
+      let startScrollLeft = 0;
+
+      itemsContainer.addEventListener('mousedown', (event) => {
+        isDragging = true;
+        startPosition = event.pageX;
+        startScrollLeft = itemsContainer.scrollLeft;
+      });
+
+      itemsContainer.addEventListener('mousemove', (event) => {
+        if (!isDragging) return;
+        event.preventDefault();
+        const distance = event.pageX - startPosition;
+        itemsContainer.scrollLeft = startScrollLeft - distance;
+      });
+
+      itemsContainer.addEventListener('mouseup', () => {
+        isDragging = false;
+      });
+
+      itemsContainer.addEventListener('mouseleave', () => {
+        isDragging = false;
+      });
+    });
+  
